181025a0be1af84ed60acdb0fe8eb6ff4eb30bc1
nbhatta28321 <nbhatta28321@ucumberlands.edu>
2024-09-08 10:35:44 -0400

    changed the pivot position

--- quickSort.py
+++ quickSort.py
@@ -3,7 +3,7 @@ import time
 import tracemalloc
 
 def quick_sort(A): #function to divide the array
-    print("A", A)
+    # print("A", A)
 
     left =[]
     middle =[]
@@ -12,7 +12,7 @@ def quick_sort(A): #function to divide the array
     if len(A) <= 1: #return when the array has only one element which is sorted
         return A
     else:
-        pivot = A[len(A)-1]
+        pivot = A[len(A) // 2]
 
         for i in A:
             if i < pivot:
@@ -28,7 +28,7 @@ def quick_sort(A): #function to divide the array
 # function to do the merge sort and print the value
 tracemalloc.start()
 start = time.time()
-quick_sort(list(range(100)))
+quick_sort(list(range(100000)))
 end = time.time()
 current, peak_memory = tracemalloc.get_traced_memory()
 tracemalloc.clear_traces()
@@ -37,7 +37,7 @@ print("Execution time for sorted numbers is {} with memory usage of {}".format(e
 
 tracemalloc.start()
 start = time.time()
-quick_sort(list(range(100, 0, -1)))
+quick_sort(list(range(10000, 0, -1)))
 end = time.time()
 current, peak_memory = tracemalloc.get_traced_memory()
 tracemalloc.clear_traces()
@@ -46,8 +46,8 @@ print("Execution time for reverse sorted numbers is {} with memory usage of {}".
 
 tracemalloc.start()
 start = time.time()
-a =quick_sort([random.randint(1, 500) for _ in range(1000)])
-print("final", a)
+a =quick_sort([random.randint(1, 500) for _ in range(100000)])
+# print("final", a)
 end = time.time()
 current, peak_memory = tracemalloc.get_traced_memory()
 tracemalloc.clear_traces()
